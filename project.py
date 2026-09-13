bb1cacd0070aff85de3e819e3409dc69d163cbfc
Lanne-0402 <honglanbd2gmail.com>
2026-09-13 23:58:57 +0700

    fix error project.py

--- project.py
+++ project.py
@@ -3,19 +3,6 @@ import sys
 from flask import Flask, jsonify, render_template, request, Response
 from flask_cors import CORS
 
-CORS(
-    app,
-    resources={
-        r"/*": {
-            "origins": [
-                "https://lanne-0402.github.io/Doan_Kythuatdohoa_N5_LanHanDiepUy/",
-                "http://127.0.0.1:5000",
-                "http://localhost:5000",
-            ]
-        }
-    },
-)
-
 # Khi chạy Python bình thường: BASE_DIR là thư mục project.
 # Khi chạy file .exe: BASE_DIR là thư mục tạm PyInstaller giải nén.
 if getattr(sys, "frozen", False):
@@ -36,7 +23,18 @@ app = Flask(
     template_folder=os.path.join(BASE_DIR, "templates"),
     static_folder=os.path.join(BASE_DIR, "static")
 )
-
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "https://lanne-0402.github.io/Doan_Kythuatdohoa_N5_LanHanDiepUy/",
+                "http://127.0.0.1:5000",
+                "http://localhost:5000",
+            ]
+        }
+    },
+)
 UNIT_SIZE = 5.0  # 1 đơn vị = 5 pixel nội bộ
 
 
